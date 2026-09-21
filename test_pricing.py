a4b9d0f86334be3e2348beb7db7c14628ccf66fd
Noe <narcisse.noe@gmail.com>
2026-09-21 15:59:33 +0200

    Update test_pricing.py

--- test_pricing.py
+++ test_pricing.py
@@ -5,3 +5,11 @@ Lancez : pytest test_pricing.py -v
 from booking import ticket_price, line_total, apply_promo, order_total
 
 # À vous d'écrire les tests.
+
+def test_ticket_price() :
+
+    assert ticket_price("vip") == 7500
+    assert ticket_price("standard") == 3500
+    assert ticket_price("early_bird") == 2495
+
+    # assert ticket_price("gollum") is ValueError
